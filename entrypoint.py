9986ff4acf080d7d5be35f1bc965208737e33f04
Roman Imankulov <roman.imankulov@gmail.com>
2019-01-07 10:43:01 +0000

    Make entrypoint print every command before running

--- entrypoint.py
+++ entrypoint.py
@@ -3,14 +3,20 @@ import sys
 import os
 from subprocess import check_call
 
+
+def run(*args):
+    print('server $', ' '.join(args))
+    return check_call(args)
+
+
 if len(sys.argv) == 1:
     raise SystemExit(f'Usage: {sys.argv[0]} web or {sys.argv[0]} <management-command>')
 
 if sys.argv[1] == 'web':
-    check_call(["./manage.py", "migrate", "--noinput"])
-    check_call(["./manage.py", "create_admin"])
-    check_call(["./manage.py", "collectstatic", "--noinput"])
-    check_call(["./manage.py", "diffsettings"])
+    run("./manage.py", "migrate", "--noinput")
+    run("./manage.py", "create_admin")
+    run("./manage.py", "collectstatic", "--noinput")
+    run("./manage.py", "diffsettings")
     os.execlp('gunicorn', 'gunicorn', 'insights.wsgi', '--bind', '0.0.0.0:8000')
 
 os.execlp("./manage.py", "./manage.py", *sys.argv[1:])
